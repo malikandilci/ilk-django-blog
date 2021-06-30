cc063009e0a2ae571261c08e19ff20b6df71ee22
Malik Kandilci <malikandil@gmail.com>
2021-06-30 17:11:41 +0300

    Django Girls app, ilk derleme

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+#from django.contrib.auth.models import User
+
+class Post(models.Model):
+    yazar = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    baslik = models.CharField(max_length=200)
+    yazi = models.TextField()
+    yaratilis_tarihi = models.DateTimeField(default=timezone.now)
+    yayinlanma_tarihi = models.DateTimeField(blank=True, null=True)
+
+    def yayinla(self):
+        self.yayinlanma_tarihi = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.baslik
